16ea1ba419f4004dd157b1bc0aa1a82a7049a7ff
Peter Absolon <peter.absolon@strv.com>
2016-07-17 19:15:56 +0200

    Hello world test

---
+++ api/actions/helloworld.js
@@ -0,0 +1,3 @@
+export default function helloworld(req) {
+  return Promise.resolve('yo')
+}
--- src/config.js
+++ src/config.js
@@ -22,7 +22,7 @@ module.exports = Object.assign({
   apiHost: process.env.APIHOST || 'localhost',
   apiPort: process.env.APIPORT,
   app: {
-    title: 'STRV React Template',
+    title: 'JurisReal',
     description: '---',
     head: {
       titleTemplate: '---',
